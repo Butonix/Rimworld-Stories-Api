48f9432fa32d670966996085fea8442870e1535f
Nicolas Machado <nicoma63@gmail.com>
2017-05-17 14:18:11 +0200

    fixed CORS

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ const app = express();
 // Enable CORS
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", CLIENT_URL);
+    res.header("Access-Control-Allow-Origin", 'https://rimworld-stories.com');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 	res.header("Access-Control-Allow-Credentials", true);
