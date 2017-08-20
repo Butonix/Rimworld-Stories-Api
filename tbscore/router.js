4f8dc1417a2abefd097e00d5d99b48222605208c
NicolasMachado <nicoma63@gmail.com>
2017-08-20 13:26:42 +0200

    added auto score request after post

--- tbscore/router.js
+++ tbscore/router.js
@@ -2,11 +2,14 @@ const express = require('express');
 const {TBScore} = require('../config/models');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer  = require('multer');
+const upload = multer({ dest: 'temp-uploads/' });
 
 // GET SCORES
 router.get('/', (req, res) => {
     TBScore
         .find()
+        .sort({ score: -1 })
         .then((scores) => {
             res.json(scores)
         })
@@ -14,14 +17,19 @@ router.get('/', (req, res) => {
 });
 
 // ADD SCORE
-router.post('/', (req, res) => {
+router.post('/', upload.none(), (req, res) => {
     TBScore
         .create({
-            username: req.body.username,
-            score: req.body.score
+            username: req.body.username || "UNKNOWN",
+            score: req.body.score || 0
         })
-        .then((scores) => {
-            res.json(scores)
+        .then(() => {
+            TBScore
+                .find()
+                .sort({ score: -1 })
+                .then((scores) => {
+                    res.json(scores)
+                })
         })
         .catch(err => {res.json({APIerror: 'Error when fetching scores: ' + err})});
 });
